5c5c439b2ef47b9219853f901274cff7c5c6c994
sugiwa <sugi.warra@gmail.com>
2021-03-23 16:51:25 +0900

    first commit

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -3,20 +3,20 @@ class FavoritesController < ApplicationController
   # POST /books/:book_id/favorites
   # POST /favorites.json
   def create
-    if !Favorite.find(book_id: params[:book_id], user_id: current_user.id).exists?
+    if !Favorite.where(book_id: params[:book_id], user_id: current_user.id).exists?
       @favorite = current_user.favorites.new(:book_id => params[:book_id])
       @favorite.save
-      redirect_to books_path
+      redirect_to request.referer
     end
   end
-
-
+  
+  
   # DELETE /books/:book_id/favorites/
   def destroy
-    if Favorite.find(book_id: params[:book_id], user_id: current_user.id).exists?
+    if Favorite.where(book_id: params[:book_id], user_id: current_user.id).exists?
       @favorite = Favorite.find_by(user_id: current_user, book_id: params[:book_id])
       @favorite.destroy
-      redirect_to books_path
+      redirect_to request.referer
     end
   end
 end
